aa9ba7b7e13e58547367cc00ccd1a0832a4c30b5
daniil <daniilkovall480@gmail.com>
2026-07-06 13:06:39 +0600

    fix: rename and add examination null

--- task05/ClassAnalyzer.cs
+++ task05/ClassAnalyzer.cs
@@ -5,30 +5,30 @@ using System.Linq;
 
 namespace _ClassAnalyzer
 {
-
-public class ClassAnalyzer
-{
-    private Type _type;
-
-    public ClassAnalyzer(Type type)
+    public class ClassAnalyzer
     {
-        _type = type;
-    }
+        private Type _type;
 
-    public IEnumerable<string> GetPublicMethods()=>_type.GetMethods().Select(met=>met.Name);
+        public ClassAnalyzer(Type type)
+        {
+            if (type == null) throw new ArgumentNullException(nameof(type));
+            _type = type;
+        }
 
-    public IEnumerable<string> GetMethodParams(string methodname)
+        public IEnumerable<string> GetPublicMethods() => _type.GetMethods().Select(met => met.Name);
+
+        public IEnumerable<string> GetMethodParams(string methodname)
         {
             MethodInfo method = _type.GetMethod(methodname);
             Type returnType = method.ReturnType;
             return method.GetParameters().Select(param => $"параметры {param.ParameterType.Name} и {param.Name}").Append($"тип {returnType.Name}");
-            
         }
-    public IEnumerable<string> GetAllFields()=> _type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-    .Select(fild => fild.Name);
 
-    public IEnumerable<string> GetProperties()=> _type.GetProperties().Select(prop => prop.Name);
-    public bool HasAttribute<T>() where T : Attribute => _type.GetCustomAttribute<T>() != null;
+        public IEnumerable<string> GetAllFields() => _type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+            .Select(fild => fild.Name);
 
-}
+        public IEnumerable<string> GetProperties() => _type.GetProperties().Select(prop => prop.Name);
+
+        public bool HasAttribute<T>() where T : Attribute => _type.IsDefined(typeof(T), inherit: true);
+    }
 }
