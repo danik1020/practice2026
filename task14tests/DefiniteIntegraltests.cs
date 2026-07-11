650071bb63835f40c6b78db8a1a9b3e535220a87
daniil <daniilkovall480@gmail.com>
2026-07-11 14:08:23 +0600

    feat: task14 solution

---
+++ task14tests/DefiniteIntegraltests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Xunit;
+using task14;
+
+
+public class DefiniteIntegralTests
+{
+    [Fact]
+    public void Solve_Sin_ReturnsCorrectValue()
+    {
+        Func<double, double> SIN = x => Math.Sin(x);
+        
+        double result = DefiniteIntegral.Solve(-1, 1, SIN, 1e-5, 8);
+        
+        Assert.Equal(0, result, 4);
+    }
+    
+    [Fact]
+    public void Solve_Linear_ReturnsCorrectValue()
+    {
+        Func<double, double> X = x => x;
+        
+        double result = DefiniteIntegral.Solve(0, 5, X, 1e-6, 8);
+        
+        Assert.Equal(12.5, result, 5);
+    }
+    
+    [Fact]
+    public void Solve_Constant_ReturnsCorrectValue()
+    {
+        Func<double, double> constant = x => 5.0;
+        
+        double result = DefiniteIntegral.Solve(0, 10, constant, 1e-4, 4);
+        
+        Assert.Equal(50, result, 4);
+    }
+    
+    [Fact]
+    public void Solve_Quad_ReturnsCorrectValue()
+    {
+        Func<double, double> quad = x => x * x;
+        
+        double result = DefiniteIntegral.Solve(0, 3, quad, 1e-5, 4);
+        
+        Assert.Equal(9, result, 4);
+    }
+    
+    [Fact]
+    public void Solve_Single_ReturnsCorrectValue()
+    {
+        Func<double, double> X = x => x;
+        
+        double result = DefiniteIntegral.Solve(0, 10, X, 1e-4, 1);
+        
+        Assert.Equal(50, result, 4);
+    }
+    
+    [Fact]
+    public void Solve_ZeroThreadsNumber_ThrowsArgumentException()
+    {
+        Func<double, double> function = x => x;
+
+        Assert.Throws<ArgumentException>(() => DefiniteIntegral.Solve(0, 1, function, 0.1, 0));
+    }
+}
