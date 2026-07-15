56e7cb6d3266ef7935cf4b3bda69dcbb94a3c7db
daniil <daniilkovall480@gmail.com>
2026-07-15 23:04:26 +0600

    feat: task17

---
+++ task17/ServerThread.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace task17
+{
+    public interface ICommand
+    {
+        void Execute();
+    }
+
+    public class ServerThread
+    {
+        BlockingCollection<ICommand> _queue = new();
+        Thread _thread;
+        Action _strategy;
+        volatile bool _isRunning = true;
+
+        public Thread UnderlyingThread => _thread;
+
+        public ServerThread()
+        {
+            _thread = new Thread(Run) { IsBackground = true };
+            _strategy = DefaultStrategy;
+        }
+
+        public void Start()
+        {
+            _thread.Start();
+        }
+
+        public void Enqueue(ICommand command)
+        {
+            if (!_queue.IsAddingCompleted)
+            {
+                _queue.Add(command);
+            }
+        }
+
+        void Run()
+        {
+            while (_isRunning)
+            {
+                try
+                {
+                    _strategy();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка в потоке: {ex.Message}");
+                }
+            }
+        }
+
+        void DefaultStrategy()
+        {
+            try
+            {
+                ICommand command = _queue.Take();
+                try
+                {
+                    command.Execute();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка в команде {command.GetType().Name}: {ex.Message}");
+                }
+            }
+            catch (InvalidOperationException)
+            {
+                _isRunning = false;
+            }
+        }
+
+        void SoftStopStrategy()
+        {
+            if (_queue.TryTake(out ICommand? command) && command != null)
+            {
+                try
+                {
+                    command.Execute();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка в команде {command.GetType().Name}: {ex.Message}");
+                }
+            }
+            else
+            {
+                _isRunning = false;
+            }
+        }
+
+        public void HardStop()
+        {
+            VerifyCurrentThread();
+            _isRunning = false;
+        }
+
+        public void SoftStop()
+        {
+            VerifyCurrentThread();
+            _strategy = SoftStopStrategy;
+        }
+
+        void VerifyCurrentThread()
+        {
+            if (Thread.CurrentThread != _thread)
+            {
+                throw new InvalidOperationException("Команда должна выполняться только внутри ServerThread");
+            }
+        }
+    }
+
+    public class HardStopCommand : ICommand
+    {
+        ServerThread _serverThread;
+
+        public HardStopCommand(ServerThread serverThread)
+        {
+            _serverThread = serverThread;
+        }
+
+        public void Execute()
+        {
+            _serverThread.HardStop();
+        }
+    }
+
+    public class SoftStopCommand : ICommand
+    {
+        ServerThread _serverThread;
+
+        public SoftStopCommand(ServerThread serverThread)
+        {
+            _serverThread = serverThread;
+        }
+
+        public void Execute()
+        {
+            _serverThread.SoftStop();
+        }
+    }
+}
---
+++ task17tests/ServerThreadtests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading;
+using Xunit;
+using task17;
+
+
+    public class SpyCommand : ICommand
+    {
+        public bool WasExecuted { get; private set; }
+        int _delay;
+
+        public SpyCommand(int delay = 0)
+        {
+            _delay = delay;
+        }
+
+        public void Execute()
+        {
+            if (_delay > 0)
+            {
+                Thread.Sleep(_delay);
+            }
+            WasExecuted = true;
+        }
+    }
+    public class ServerThreadTests
+    {
+        [Fact]
+        public void HardStopCommand_ShouldStopThread_IgnoringRemainingCommands()
+        {
+            var server = new ServerThread();
+            var cmd1 = new SpyCommand();
+            var hardStop = new HardStopCommand(server);
+            var cmd2 = new SpyCommand();
+            server.Enqueue(cmd1);
+            server.Enqueue(hardStop);
+            server.Enqueue(cmd2);
+            server.Start();
+            server.UnderlyingThread.Join(1000);
+            Assert.True(cmd1.WasExecuted);
+            Assert.False(cmd2.WasExecuted);
+            Assert.False(server.UnderlyingThread.IsAlive);
+        }
+
+        [Fact]
+        public void SoftStopCommand_ShouldProcessAllQueuedCommands()
+        {
+            var server = new ServerThread();
+            var cmd1 = new SpyCommand();
+            var softStop = new SoftStopCommand(server);
+            var cmd2 = new SpyCommand();
+            server.Enqueue(cmd1);
+            server.Enqueue(softStop);
+            server.Enqueue(cmd2);
+            server.Start();
+            server.UnderlyingThread.Join(1000);
+            Assert.True(cmd1.WasExecuted);
+            Assert.True(cmd2.WasExecuted);
+            Assert.False(server.UnderlyingThread.IsAlive);
+        }
+
+        [Fact]
+        public void Commands_ShouldThrowException_ExecutedInWrongThread()
+        {
+            var server = new ServerThread();
+            server.Start();
+            var hardStop = new HardStopCommand(server);
+            var softStop = new SoftStopCommand(server);
+            Assert.Throws<InvalidOperationException>(() => hardStop.Execute());
+            Assert.Throws<InvalidOperationException>(() => softStop.Execute());
+            server.Enqueue(new HardStopCommand(server));
+            server.UnderlyingThread.Join(500);
+        }
+    }
